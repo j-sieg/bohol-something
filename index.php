3fe6c2971ba427d7c41d13be87844dfd714f8b50
the-siegmeister <siegmeister@protonmail.com>
2021-02-14 04:26:16 +0800

    Update the index page

--- index.php
+++ index.php
@@ -2,8 +2,112 @@
   include('./views/header.php');
 ?>
 
-<main class='mt-4'>
-  <h1 class='text-center text-danger'> Come to Bohol </h1>
+<main class='container mt-4'>
+
+  <section class="text-center"> 
+    <h1 class='fw-bolder text-decoration-underline text-center'>COME TO BOHOL</h1>
+    <p class='fst-italic fw-bolder text-decoration-underline'>EXPERIENCE THE GREATNESS OF BOHOL</p>
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/u62-kCDwAmA"
+      frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>
+    </iframe>
+    <hr>
+  </section> <!-- center -->
+
+  <section class="row">
+    <h1 class='mb-4 fw-bolder text-decoration-underline text-center'>WHATS NEW IN BOHOL?</h1>
+    <div class="col-md-4">
+      <h3>BOHOL READY TO WELCOME BACK TOURISTS</h3>
+      <p>
+        Cebu City, Philippines — Bohol is now ready to reopen and welcome tourists back to
+        its shores.
+      </p>
+      <p>
+        The announcement came during a digital conference with members of the press as the
+        province, together with tourism stakeholders launched the “Balik sa Bohol: Bohol
+        Online Sale” an online sale where hotels, resorts, and tour operators offer massive
+        price drops to lure tourists back to the island province.
+      </p>
+      <p>
+        Bohol Governor Arthur Yap explained that the province has long been ready for the
+        return of tourists back to the island as health and safety protocols have been
+        modified to fit the needs of tourists in co-relation to the guidelines set by the
+        COVID-19 Inter-Agency Task Force for the Management of Emerging Infectious Diseases
+        (IATF).
+      </p>
+      <p>
+        According to Yap, with the success of the Philippine Travel Exchange (PHITEX) in
+        September; and the Philippine Tourism Destination Inventory and MICE+E Bohol
+        Familiarization Trip both held in the island in December, the two events served
+        as thrust as they open their shores to tourists.
+      </p>
+      <p>
+        Panglao Mayor Leonila Montelo said that Bohol’s premier tourist destination suffered
+        huge losses during the lockdown that turned the once-bustling island into a “ghost
+        town”. <a href='https://cebudailynews.inquirer.net/362975/bohol-ready-to-welcome-back-tourists'>Source</a>.
+      </p>
+    </div> <!-- end -->
+    
+    <div class="col-md-4">
+      <h3>WHAT TO EXPECT FROM THE "NEW NORMAL" IN BOHOL?</h3>
+      <p>
+        PANGLAO, Bohol -- The island province of Bohol is gearing up to finally welcome
+        leisure tourists starting December 15, 2020 including those from areas under
+        general community quarantine (GCQ) areas like Metro Manila.
+      </p>
+      <p>
+        Bohol Governor Arthur Yap confirmed that they will be accepting frequent independent
+        travelers of all ages, including senior citizens and minors, in a bid to jumpstart the
+        local economy.
+      </p>
+      <p>
+        Tourism Secretary Bernadette Romulo-Puyat welcomed the development of easing age
+        restrictions for travel, saying that Christmas is just around the corner and that
+        Filipinos really travel together as a family.
+      </p>
+      <p>
+        Tour operators and members of the media got to experience first-hand the steps the
+        provincial government of Bohol is taking to fight COVID-19 while cautiously reopening
+        the island to tourists.
+      </p>
+      <p>
+        If you’re thinking of going to Bohol soon, you need to know the requirements.
+        <a href='https://news.abs-cbn.com/life/12/12/20/what-to-expect-from-the-new-normal-travel-in-bohol'>
+        Source</a>.
+      </p>
+    </div> <!-- end -->
+
+    <div class="col-md-4">
+      <h3>BOHOL SCRAPS QUARANTINE PROCEDURES FOR ARRIVING TOURISTS</h3>
+      <p>
+        THE province of Bohol has eased up on some of its protocols meant
+        to contain the spread of Covid-19, in a bid to inject life into its
+        moribund tourism industry.
+      </p>
+      <p>
+        For one, tourists are not required to quarantine for five to 14 days
+        upon arrival.  In an online news briefing for the Balik sa Bohol online
+        sale, Bohol Gov. Arthur Yap added, tourists only have to present a
+        “negative RT-PCR test result, with the swab taken within 72 hours to
+        travel time; confirmed booking at a Department of Tourism (DOT)-accredited
+        hotel, and must register at http://tourism.bohol.gov.ph. For those going on
+        tour, a confirmed tour booking through a DOT-accredited tour operator.”
+      </p>
+      <p>
+        He also said, the province is now accepting RT-PCR saliva testing to lower
+        travel costs and encourage more tourists to visit, “and we have devised a
+        ‘bubble tourism’ system to allow greater flexibility for travel.”
+      </p>
+      <p>
+        While Panglao Island and the municipality of Anda are the only areas officially
+        open for domestic tourism, Yap explained the “bubble tourism” concept will
+        still allow tourists to “visit attractions along the way even if municipalities
+        are not officially open.” Such itineraries will have to be booked through
+        accredited tour operators enumerated on the Bohol province portal.
+        <a href='https://businessmirror.com.ph/2021/02/11/bohol-scraps-quarantine-procedures-for-arriving-tourists/'>Source</a>.
+      </p>
+    </div> <!-- end -->
+  </section>
+
 </main>
 
 <?php
